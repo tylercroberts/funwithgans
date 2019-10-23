8034fd3b6deb843c9fb16fe0bbb006c1db10f722
Tyler <tcroberts@live.ca>
2019-10-23 14:52:22 -0700

    Refactor functions between utils and main script

--- dcgan/src/__init__.py
+++ dcgan/src/__init__.py
@@ -24,114 +24,16 @@ import torchvision.utils as vutils
 
 # Self imports:
 from dcgan.src.networks import Generator, Discriminator, LATENT_SHAPE
-from dcgan.src.utils import weights_init
+from dcgan.src.utils import logging_wrapper, parse_args, set_flags
 
-
-def logging_wrapper():
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(formatter)
-    console_handler.setLevel(logging.INFO)
-    root.addHandler(console_handler)
-    root.info("Console logging successfully configured")
-
-    return root
-
-
-def overwrite_args_with_config(args):
-    with open(args.config, 'r') as f:
-        json_config = json.loads(f.read())
-
-    for k, v in json_config.items():
-        args.__setattr__(k, v)
-
-    return args
-
-def parse_args(log):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config',
-                        help='Path to config file which contains filepaths and other arguments',
-                        default=False,
-                        type=str)
-    parser.add_argument("--storage-dir",
-                        help="Storage directory for files, default: $CWD",
-                        default=os.path.join(os.getcwd(), '..'))
-    parser.add_argument("--model-dir",
-                        help='Directory in which to store models after training',
-                        default=os.path.join(os.getcwd(), '..', 'models'))
-    parser.add_argument('--image-dir',
-                        help='Directory in which to store images generated during training.',
-                        default=os.path.join(os.getcwd(), 'dcgan', 'out'))
-    parser.add_argument('--log-dir',
-                        help='Directory in which to store log files.',
-                        default='logs')
-    parser.add_argument("--reproducible",
-                        help='Flag to determine whether to set a predetermined seed for training',
-                        type=bool)
-    parser.add_argument("--loader-workers",
-                        help='Workers to use for dataloading',
-                        default=2,
-                        type=int)
-    parser.add_argument("--batch-size",
-                        help='Batch size during training',
-                        default=128,
-                        type=int)
-    parser.add_argument("--image-dim",
-                        help='Size of images to generate',
-                        default=64,
-                        type=int)
-    parser.add_argument("--epochs",
-                        help='Number of times to iterate over dataset during training',
-                        default=10,
-                        type=int)
-    parser.add_argument("--lr",
-                        help='Learning rate for ADAM optimizer',
-                        default=0.0002,
-                        type=float)
-    parser.add_argument("--beta",
-                        help='Value to use for beta1 parameter for ADAM optimizer',
-                        default=0.999,
-                        type=float)
-    parser.add_argument("--ngpu",
-                        help='Number of times to iterate over dataset during training',
-                        default=1,
-                        type=int)
-
-    parsed_args = parser.parse_args()
-    if Path(parsed_args.config).exists():
-        logger.info(f"Overwriting any command-line arguments with config file found at "
-                    f"{os.getcwd()}\\{parsed_args.config}")
-        parsed_args = overwrite_args_with_config(parsed_args)
-
-    # Update the logger with filepath if necessary
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler(f"{parsed_args.log_dir}/celebagan_{time.time()}.log")
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.DEBUG)
-    log.addHandler(file_handler)
-    log.info("File logging successfully configured")
-
-    for k, v in parsed_args.__dict__.items():
-        log.debug(f"{k}: {v}")
-
-    return parsed_args, log
-
-
-def set_flags(args):
-    LOADER_WORKERS = args.loader_workers
-    BATCH_SIZE = args.batch_size
-    IMG_DIM = args.image_dim
-    EPOCHS = args.epochs
-    LR = args.lr
-    BETA1 = args.beta
-    NGPU = args.ngpu
-    logger.info(f"Script flags set are: loader_workers: {LOADER_WORKERS}, batch_size: {BATCH_SIZE},"
-                f" img_dim: {IMG_DIM}, epochs: {EPOCHS}, lr: {LR}, beta1: {BETA1}, ngpu: {NGPU}")
-    return LOADER_WORKERS, BATCH_SIZE, IMG_DIM, EPOCHS, LR, BETA1, NGPU
+def weights_init(m):
+    """ Basic weight initialization for DCGAN"""
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
 
 if __name__ == '__main__':
@@ -139,7 +41,7 @@ if __name__ == '__main__':
 
     args, logger = parse_args(logger)
 
-    LOADER_WORKERS, BATCH_SIZE, IMG_DIM, EPOCHS, LR, BETA1, NGPU = set_flags(args)
+    LOADER_WORKERS, BATCH_SIZE, IMG_DIM, EPOCHS, LR, BETA1, NGPU = set_flags(args, logger)
 
     random_seed = 903 if bool(args.reproducible) is True else random.randint(1, 100000)
     logger.info(f"Random seed is {random_seed}")
--- dcgan/src/utils.py
+++ dcgan/src/utils.py
@@ -1,11 +1,115 @@
-import torch.nn as nn
+import os
+import sys
+import json
+import time
+import logging
+import argparse
+from pathlib import Path
 
 
-def weights_init(m):
-    """ Basic weight initialization for DCGAN"""
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm") != -1:
-        nn.init.normal_(m.weight.data, 1, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+def set_flags(args, logger):
+    LOADER_WORKERS = args.loader_workers
+    BATCH_SIZE = args.batch_size
+    IMG_DIM = args.image_dim
+    EPOCHS = args.epochs
+    LR = args.lr
+    BETA1 = args.beta
+    NGPU = args.ngpu
+    logger.info(f"Script flags set are: loader_workers: {LOADER_WORKERS}, batch_size: {BATCH_SIZE},"
+                f" img_dim: {IMG_DIM}, epochs: {EPOCHS}, lr: {LR}, beta1: {BETA1}, ngpu: {NGPU}")
+    return LOADER_WORKERS, BATCH_SIZE, IMG_DIM, EPOCHS, LR, BETA1, NGPU
+
+
+def logging_wrapper():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)
+    root.addHandler(console_handler)
+    root.info("Console logging successfully configured")
+
+    return root
+
+
+def _overwrite_args_with_config(args):
+    with open(args.config, 'r') as f:
+        json_config = json.loads(f.read())
+
+    for k, v in json_config.items():
+        args.__setattr__(k, v)
+
+    return args
+
+
+def parse_args(log):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config',
+                        help='Path to config file which contains filepaths and other arguments',
+                        default=False,
+                        type=str)
+    parser.add_argument("--storage-dir",
+                        help="Storage directory for files, default: $CWD",
+                        default=os.path.join(os.getcwd(), '..'))
+    parser.add_argument("--model-dir",
+                        help='Directory in which to store models after training',
+                        default=os.path.join(os.getcwd(), '..', 'models'))
+    parser.add_argument('--image-dir',
+                        help='Directory in which to store images generated during training.',
+                        default=os.path.join(os.getcwd(), 'dcgan', 'out'))
+    parser.add_argument('--log-dir',
+                        help='Directory in which to store log files.',
+                        default='logs')
+    parser.add_argument("--reproducible",
+                        help='Flag to determine whether to set a predetermined seed for training',
+                        type=bool)
+    parser.add_argument("--loader-workers",
+                        help='Workers to use for dataloading',
+                        default=2,
+                        type=int)
+    parser.add_argument("--batch-size",
+                        help='Batch size during training',
+                        default=128,
+                        type=int)
+    parser.add_argument("--image-dim",
+                        help='Size of images to generate',
+                        default=64,
+                        type=int)
+    parser.add_argument("--epochs",
+                        help='Number of times to iterate over dataset during training',
+                        default=10,
+                        type=int)
+    parser.add_argument("--lr",
+                        help='Learning rate for ADAM optimizer',
+                        default=0.0002,
+                        type=float)
+    parser.add_argument("--beta",
+                        help='Value to use for beta1 parameter for ADAM optimizer',
+                        default=0.999,
+                        type=float)
+    parser.add_argument("--ngpu",
+                        help='Number of times to iterate over dataset during training',
+                        default=1,
+                        type=int)
+
+    parsed_args = parser.parse_args()
+    if Path(parsed_args.config).exists():
+        log.info(f"Overwriting any command-line arguments with config file found at "
+                 f"{os.getcwd()}\\{parsed_args.config}")
+        parsed_args = _overwrite_args_with_config(parsed_args)
+
+    # Update the logger with filepath if necessary
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(f"{parsed_args.log_dir}/celebagan_{time.time()}.log")
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+    log.addHandler(file_handler)
+    log.info("File logging successfully configured")
+
+    for k, v in parsed_args.__dict__.items():
+        log.debug(f"{k}: {v}")
+
+    return parsed_args, log
